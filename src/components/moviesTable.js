6b5eff8095e40a8aada2bbf040d7f6f67613d47f
Priya Ravi <priyaravi23@gmail.com>
2020-03-23 16:20:28 -0700

    implemented navbar and routing

--- src/components/moviesTable.js
+++ src/components/moviesTable.js
@@ -1,28 +1,22 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import Like from '../components/common/like';
 import Table from '../components/common/table';
 
 class MoviesTable extends Component {
     columns = [
-        { path: 'title', label: 'Title'},
-        { path: 'genre.name', label: 'Genre'},
-        { path: 'numberInStock', label: 'Stock'},
-        { path: 'dailyRentalRate', label: 'Rate'},
         {
-            key: 'like',
-            content: movie => (
-                <Like
-                    liked={movie.liked}
-                    onClick={() => this.props.onLike(movie)} />
-            )
+            path: "title",
+            label: "Title",
+            content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
         },
+        { path: "genre.name", label: "Genre" },
+        { path: "numberInStock", label: "Stock" },
+        { path: "dailyRentalRate", label: "Rate" },
         {
-            key: 'delete',
+            key: "like",
             content: movie => (
-                <button
-                    onClick={() => this.props.onDelete(movie)}
-                    className="btn btn-danger btn-sm">Delete
-                </button>
+                <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
             )
         }
     ];
