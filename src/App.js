871c24b3762dd13a812733d8eed674e622644180
Priya Ravi <priyaravi23@gmail.com>
2020-05-04 17:18:59 -0700

    connecting client app to backend services

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Movies from "./components/movies";
 import MovieForm from "./components/movieForm";
 import TvSeries from "./components/tvSeries";
@@ -8,12 +9,14 @@ import NavBar from "./components/navbar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
 import MovieCarousel from './components/movieCarousel';
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <React.Fragment>
+                <ToastContainer />
                 <NavBar />
                 <Switch>
                     <Route path="/register" component={RegisterForm} />
